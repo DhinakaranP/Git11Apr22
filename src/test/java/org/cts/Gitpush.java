3bba5868a95a5b28a5da6ccbbbe7b0f92fbe5393
DhinakaranP <sarathidhina@gmail.com>
2022-04-11 23:06:48 +0530

    Maven

---
+++ src/test/java/org/cts/Gitpush.java
@@ -0,0 +1,28 @@
+package org.cts;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Gitpush {
+	public static void main(String[] args) {
+		String s="welcome to java java class to class welcome";
+		
+		String[] sp = s.split(" ");
+		
+		Map<String, Integer> mp=new LinkedHashMap<String, Integer>();
+		
+		for (int i = 0; i < sp.length; i++) {
+			
+			String s1=sp[i];
+			
+			if(mp.containsKey(s1)) {
+				Integer x = mp.get(s1);
+				mp.put(s1, x+1);
+			}
+			else {
+				mp.put(s1, 1);
+			}
+			}
+		System.out.println(mp);
+		}
+	}
